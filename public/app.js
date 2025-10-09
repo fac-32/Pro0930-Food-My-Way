2b20212873252c85d1677c3fde885f12a79645f9
krilklem <rafi.panyotova@gmail.com>
2025-10-09 15:35:48 +0100

    fix: remove redundant commented out code block

--- public/app.js
+++ public/app.js
@@ -51,27 +51,4 @@ document.addEventListener("DOMContentLoaded", () => {
             ingredient.reportValidity();
         }
     });
-
-    // button.addEventListener('click', async () => {
-    //     try {
-    //         const response = await fetch('/api/meals'); // calls your backend
-    //         const data = await response.json();
-
-    //         container.innerHTML = ''; // clear previous
-    //         data.meals.slice(0, 4).forEach(meal => {
-
-    //         const card = document.createElement('div');
-    //         card.classList.add('meal-card');
-    //         card.innerHTML = `
-    //                 <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
-    //                 <h3>${meal.strMeal}</h3>
-    //                 `;
-
-    //         container.appendChild(card);
-            
-    //         });
-    //     } catch (error) {
-    //         container.innerHTML = `<p>Error: ${error}</p>`;
-    //     }
-    // });
 });
